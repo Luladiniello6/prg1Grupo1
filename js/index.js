2c8f17b1d8f20c23b14ff150ba3fc5ec2bbfb00c
Luladiniello6 <ldiniello@udesa.edu.ar>
2024-11-19 21:58:20 -0300

    cambios index

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 fetch('https://dummyjson.com/recipes')
+
+function validarFormulario(event) {
+    const campoBusqueda = document.getElementById('busqueda');
+    const mensajeError = document.getElementById('error');
+
+    mensajeError.textContent = '';
+
+    if (campoBusqueda.value.trim() === '') {
+        mensajeError.textContent = 'El campo no puede estar vacío.';
+        return false;
+    }
+    if (campoBusqueda.value.trim().length < 3) {
+        mensajeError.textContent = 'La búsqueda debe tener al menos 3 caracteres.';
+        return false;
+    }
+    return true;
