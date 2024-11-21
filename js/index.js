3e24013d08b5887a7467c6796e9f440fed48842c
SolAlak <salak@udesa.edu.ar>
2024-11-21 09:11:17 -0300

    ultimo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
+fetch("https://dummyjson.com/recipes")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log (data)
+    let recetas = data.recipes
+    let receta = " "
+    let listaRecetas = document.querySelector('.lista-recetas');
+    
+      for (let i = 0; i < recetas.length; i++) {
+        receta += `
+                  <article class="cadaReceta">
+                    <img src=${recetas[i].image} alt="" ></img>
+                    <h1> ${recetas[i].name}</h1>
+                    <p> ${recetas[i].difficulty}</p>
+                    <a href="/recipes/${recetas[i].id}"></a>
+                  </article>
+        `
+      }
+      
+      console.log(receta);
+      listaRecetas.innerHTML = receta
+  })
 
+  .catch(function(error){
+    console.log("error;",error)
+  });
