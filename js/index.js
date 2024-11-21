6bc558135ec145099842fd87b97e794e5ccac94b
SolAlak <salak@udesa.edu.ar>
2024-11-21 16:42:08 -0300

    recetas cargar mas

--- js/index.js
+++ js/index.js
@@ -1,32 +1,39 @@
 fetch("https://dummyjson.com/recipes")
-  .then(function(response){
-    return response.json();
+  .then(function (response) {
+    return response.json(); 
   })
-  .then(function(data){
-    console.log (data)
-    let recetas = data.recipes
-    let receta = " "
-    let listaRecetas = document.querySelector('.lista-recetas');
-    listaRecetas.style.distplay = "flex"
-    listaRecetas.style.flexWrap = "wrap" 
-    
+  .then(function (data) {
+    console.log(data);
+    let recetas = data.recipes;
+    let listaRecetas = document.querySelector(".lista-recetas"); 
+    let recetasMostradas = 0; 
+    const recetasPorPagina = 10;
 
-      for (let i = 0; i < recetas.length; i++) {
+    function cargarRecetas() {
+      let receta = "";
+      for (let i = recetasMostradas; i < recetasMostradas + recetasPorPagina; i++) {
+        if (i >= recetas.length) break;
+
         receta += `
-                  <article class="cadaReceta">
-                    <img src=${recetas[i].image} alt="" ></img>
-                    <h1> ${recetas[i].name}</h1>
-                    <p> ${recetas[i].difficulty}</p>
-                    <a href="/recipes/${recetas[i].id}"></a>
-                  </article>
-        `
+          <article class="cadaReceta">
+            <img src=${recetas[i].image} alt="Imagen de receta"></img>
+            <h1>${recetas[i].name}</h1>
+            <p>${recetas[i].difficulty}</p>
+            <a href="/recipes/${recetas[i].id}">Ver más</a>
+          </article>
+        `;
       }
-      
-      console.log(receta);
-      listaRecetas.innerHTML = receta
-    })
-    
-    .catch(function(error){
-      console.log("error;",error)
-    });
-    
+      listaRecetas.innerHTML += receta;
+      recetasMostradas += recetasPorPagina;
+
+      if (recetasMostradas >= recetas.length) {
+        botonCargarMas.style.display = "none";
+      }
+    }
+    let botonCargarMas = document.querySelector(".cargar-mas");
+    botonCargarMas.addEventListener("click", cargarRecetas);
+    cargarRecetas();
+  })
+  .catch(function (error) {
+    console.log("Error:", error);
+  });
